b235514f2bb2abb10a926fc98adaa81c71ccb9db
Dan Häberlein <dan.haeberlein@googlemail.com>
2015-04-24 14:59:00 +0200

    #2 Erstellung von Mock-RESTful websources (fuer Suchvorschlaege) begonnen

---
+++ app/controllers/SearchSuggestionControler.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api._
+import play.api.libs.json.Json
+import play.api.mvc._
+
+
+/**
+ * Created by Dan Häberlein on 24.04.15.
+ */
+object SearchSuggestionControler extends Controller {
+  def index = Action(play.api.mvc.BodyParsers.parse.json) { request =>
+    val transferable: Map[String, String] = Map("message" -> "This service is under construction")
+    Ok(Json.toJson(transferable))
+  }
+}
