5e9aaa5b56ea23c58e61023970a01b83d77ec089
Yves Bugge <yab07290@googlemail.com>
2015-05-19 14:53:49 +0200

    #21 sparql navbar fertig

--- app/controllers/LdbController.scala
+++ app/controllers/LdbController.scala
@@ -5,7 +5,8 @@ import play.api.mvc._
 object LdbController extends Controller {
 
   def index = Action {
-    Ok(views.html.ldb_harness("Linked Data Browser"))
+    Ok(views.html.ldb_harness("Linked Open Data Browser")("default endpoint :)"))
   }
+
 
 }
