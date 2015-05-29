e19a00969285a9ca05d753d25db70adebe60d3af
Yves Bugge <yab07290@googlemail.com>
2015-05-29 15:01:09 +0200

    implement searchsuggestions view

--- public/javascripts/searchdirective.js
+++ public/javascripts/searchdirective.js
@@ -4,9 +4,6 @@
 'use strict';
 
 angular.module('ldbSearchDirective', [])
-    .controller('ldbSearchDirectiveController', ['$scope', function($scope) {
-        // not used in the moment
-    }])
     .directive('ldbSearch', function() {
         return {
             restrict : 'AEC',
@@ -15,4 +12,27 @@ angular.module('ldbSearchDirective', [])
             },
             templateUrl: 'assets/angular-templates/ldb_searchtemplate.html'
         };
-    });
+    }).controller('ldbSearchDirectiveController', ['$scope', '$http', function($scope,$http) {
+
+        $scope.onSelect = function ($item, $model, $label) {
+            $scope.$item = $item;
+            $scope.$model = $model;
+            $scope.$label = $label;
+            alert($item.uri);
+        };
+
+        $scope.$watch('searchString',function (newValue, oldValue) {
+           console.log('newValue=' + newValue);
+           $scope.searchSuggestionList = [];
+           if(newValue !== undefined && newValue.length >= 2){
+                //load suggestion from server
+                $http.get("/searchsuggestion",{params:{query:newValue}})
+                    .success(function(response) {
+                        $scope.searchSuggestionList = JSON.parse( response);
+                    });
+
+           } else if (newValue !== undefined && newValue.length < 2 ){
+                $scope.searchSuggestionList = [];
+           }
+        });
+}]);
