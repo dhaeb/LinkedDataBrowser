766b42c2927b0e9d25336e7052e8c144a8eb05d5
dhaeb <dan.haeberlein@googlemail.com>
2015-05-31 13:36:27 +0200

    add library test for apache jena with focus on sparql construct queries

--- test/de/aksw/library/JenaTurtleLibraryTest.java
+++ test/de/aksw/library/JenaTurtleLibraryTest.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import com.hp.hpl.jena.query.*;
 import org.apache.jena.riot.RDFDataMgr;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -17,15 +17,16 @@ import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.SimpleSelector;
 
-@Ignore
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
 public class JenaTurtleLibraryTest {
-	// needs at least 4 GB RAM (-Xmx4g -Xms4g as VM startup parameters)
-	private Model model = RDFDataMgr.loadModel("en_surface_forms.ttl");
+	private Model model = RDFDataMgr.loadModel("test-surface-forms.ttl");
 	final String dcat = "http://www.w3.org/2004/02/skos/core#altLabel";
 	Property p = model.createProperty(dcat);
 
 	@Test
-	public void testJenaWithBigFile() throws Exception {
+	public void testJena() throws Exception {
 		System.out.println(model.size());
 		ResIterator datasets = model.listSubjects();
 		long count = 0, labels = 0;
@@ -50,5 +51,22 @@ public class JenaTurtleLibraryTest {
 		SimpleSelector s = new SimpleSelector(r,(Property)null,(RDFNode)null);
 		Model simpleModel = ModelFactory.createDefaultModel().add(model.listStatements(s));
 		simpleModel.write(Files.newBufferedWriter(Paths.get("./test.ttl")), "TURTLE");
+	}
+
+
+	@Test
+	public void executeSparqlQuery(){
+		Query q = QueryFactory.create("PREFIX dbres: <http://dbpedia.org/resource/>\n" +
+				"\n" +
+				"CONSTRUCT{ <http://dbpedia.org/resource/SWAT> ?p ?o}\n" +
+				"WHERE {\n" +
+				"  \n" +
+				"  dbres:SWAT ?p ?o .\n" +
+
+				"}");
+		Model result = QueryExecutionFactory.create(q, model).execConstruct();
+		System.out.println(result.toString());
+		assertFalse("the constructed model should be non empty!", result.isEmpty());
+		assertEquals(17, result.size());
 	}
 }
