8a2a98c0a4995f3d13c575688cb768f65821fd3c
dhaeb <dan.haeberlein@googlemail.com>
2015-06-05 16:25:22 +0200

    renamed main controller

--- app/controllers/LdbMainController.scala
+++ app/controllers/LdbMainController.scala
@@ -3,7 +3,7 @@ package controllers
 import de.aksw.Constants
 import play.api.mvc._
 
-object LdbController extends Controller {
+object LdbMainController extends Controller {
 
   def index = Action {
     Ok(views.html.ldb_harness("Linked Open Data Browser")(Constants.DBPEDIA_ENDPOINT))
