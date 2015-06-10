5dc1a8e6832af507d813b624adf80071a3894508
dhaeb <dan.haeberlein@googlemail.com>
2015-06-10 16:18:52 +0200

    finished picture google request with jsonp

--- public/widgets/picture/src/picture_widget.js
+++ public/widgets/picture/src/picture_widget.js
@@ -1,21 +1,30 @@
 angular.module('lodb.widget.main.picture', [])
-.controller('pictureCtrl', function($scope,responseData,$http) {
-    $scope.myInterval = 5000;
-    var pictures = angular.fromJson(responseData);
-    var slides = $scope.slides = [];
-    for(var picture in pictures){
-        slides.push({image: pictures[picture],text:pictures[picture]});
-    }
-    var googleApiUrl='http://ajax.googleapis.com/ajax/services/search/images';
-    $http.get(googleApiUrl,{params:{'v' : '1.0',
-                        'q':'Mariah Carey',
-                },headers:{
-                    'Access-Control-Allow-Origin': "http://ajax.googleapis.com",
+    .controller('pictureCtrl', function ($scope, responseData, $http, config) {
+        $scope.myInterval = 5000;
+        var pictures = angular.fromJson(responseData);
+        var slides = $scope.slides = [];
+        for (var picture in pictures) {
+            slides.push({image: pictures[picture], text: pictures[picture]});
+        }
+        var googleApiUrl = 'http://ajax.googleapis.com/ajax/services/search/images';
+        var query = config.uri.substring(config.uri.lastIndexOf("/") + 1, config.uri.length);
+        console.log(query);
+        $http.jsonp(googleApiUrl, {
+            params: {
+                'v': '1.0',
+                'q': query,
+                'callback': 'JSON_CALLBACK'
+            }, headers: {
+                'Access-Control-Allow-Origin': "http://ajax.googleapis.com",
+            }
+        })
+            .success(function (data) {
+                console.log(data);
+                if (data) {
+                    var picList= data.responseData.results;
+                    for (var pic in picList) {
+                        slides.push({image: picList[pic].url, text: picList[pic].url});
+                    }
                 }
-                })
-                .success(function(data){
-                    if (data){
-                      var picJson=angular.fromJson(data);
-                      $scope.test=picJson;
-                    }});
- });
+            });
+    });
